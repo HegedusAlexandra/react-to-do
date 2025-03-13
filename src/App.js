15d4b033a56e52458ec1440663a06b7451ef5bb1
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-03-13 10:33:09 +0100

    style correction

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
     >
       {todo.text}
       <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
+        <button onClick={() => completeTodo(index)}>completed</button>
+        <button onClick={() => removeTodo(index)}>delete</button>
       </div>
     </div>
   );
@@ -27,7 +27,8 @@ function TodoForm({ addTodo }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className='todo' onSubmit={handleSubmit}>
+      <p> ÚJ FELADAT: </p>
       <input
         type="text"
         className="input"
